782555e3327ba6f04db097ca21a78ff168bb1c4a
abdullojr <iamnozimjon@gmail.com>
2023-07-29 11:50:15 +0500

    refactor: deleted unused imports and comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,46 @@ import If from '@/components/If'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home () {
+export default function Home() {
   return (
     <div className="p-[50px]">
-      {/* <Icon name="deleteoutline" size={50} /> */}
-      <Button label="Button"  type='primary' />
+      <Button
+        label="Append"
+        type="primary"
+        append={<Icon name="favoriteborder" />}
+      />
+      <br />
+      <Button
+        label="Append"
+        type="outline"
+        append={<Icon name="favoriteborder" />}
+      />
+      <br />
 
-      <If condition={false} otherChildren={<h1>No</h1>}>
-        <p>Hey</p>
-      </If>
+      <Button
+        label="Prepend"
+        type="primary"
+        prepend={<Icon name="favoriteborder" />}
+      />
+      <br />
+
+      <Button
+        label="Prepend"
+        type="outline"
+        prepend={<Icon name="favoriteborder" />}
+      />
+      <br />
+
+      <Button label="More" type="primary" more />
+      <br />
+
+      <Button label="More" type="outline" more />
+      <br />
+
+      <Button label="Simple" type="primary" />
+      <br />
+
+      <Button label="Simple" type="outline" />
     </div>
   )
 }
